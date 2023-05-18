4744d0fc269c0a8490e8f13f31e6536c437b14eb
Armans0701~ <sikder89098@gmail.com>
2023-05-19 03:12:27 +0600

    added navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
-
+import img1 from '../../assets/blonde-boy-sitting-holding-dinosaur-toy-staring.jpg'
 const Home = () => {
     return (
         <div>
             <h2 className='text-center'>This is Home</h2>
-            <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-            {/* <div className="carousel w-full">
-                <div id="item1" className="carousel-item w-full">
-                    <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full h-1/2">
+                    <img src={img1} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
-                    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://img.freepik.com/free-photo/cute-baby-with-toys_624325-3269.jpg?w=740&t=st=1684444189~exp=1684444789~hmac=5ff7d42a10055c796e84167c2c7a03fcfee43615074b017e4fa59d3d500d2e5c" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item3" className="carousel-item w-full">
+                <div id="slide3" className="carousel-item relative w-full">
                     <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item4" className="carousel-item w-full">
+                <div id="slide4" className="carousel-item relative w-full">
                     <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
             </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-                <a href="#item4" className="btn btn-xs">4</a>
-            </div> */}
         </div>
     );
 };
